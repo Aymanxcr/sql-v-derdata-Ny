8fc350fc93e7e732a71b68ea44038aedfb5580e3
Aymanxcr <mursalayman@gmail.com>
2024-12-07 23:09:14 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Väderdata.Core;
+using Väderdata.UI;
+
+
+
+ExcelData excel = new ExcelData();
+excel.ReadFileAndUploadToDB(@"C:\Users\cr7gr\Desktop\ProjectData\TempFuktData.csv");
+Menu.DisplayAndSelectFromMenu();
